042625b8956182a700d2932de9fa67d06de71bae
crbaker4 <chaserthebaker4@gmail.com>
2026-02-10 18:00:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -31,23 +31,4 @@ scenarios = [
 "choices": [
 ("Give them a raise", {"cash": -5000, "stress": -3}),
 ("Let them leave", {"stress": 5}),
-("Hire replacement", {"cash": -3000}),
-("Ignore it", {"stress": 8})
-]
-},
-
-{
-"text": "Your brother wants to expand fast.",
-"choices": [
-("Open second location", {"cash": -80000, "locations": 1, "stress": 10}),
-("Wait and grow", {"stress": -2}),
-("Take bank loan", {"cash": 50000, "stress": 5}),
-("Say no", {"stress": 3})
-]
-},
-
-{
-"text": "Schedule is overloaded.",
-"choices": [
-("Hire associate", {"cash": -7000, "stress": -4}),
-("Work more", {"stress":
+("Hire replacement", {"c
